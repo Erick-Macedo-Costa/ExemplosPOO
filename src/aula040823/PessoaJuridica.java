24addb0fc4f6afc6df088edfdefc4532ed32bb4f
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-04 08:39:13 -0300

    adicionado atributos

--- src/aula040823/PessoaJuridica.java
+++ src/aula040823/PessoaJuridica.java
@@ -1,4 +1,11 @@
 package aula040823;
 
+import java.time.LocalDate;
+
 public class PessoaJuridica {
+    String nomeFantasia;
+    String razaoSocial;
+    LocalDate dataFundacao;
+    String cnpj;
+
 }
