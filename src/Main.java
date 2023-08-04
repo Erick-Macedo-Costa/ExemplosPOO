2dceb5eabb6bf09f3a4c9e2b39f9c771e803b578
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-04 09:57:08 -0300

    adicionado a main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import aula040823.PessoaFisica;
+import aula280723.Aluno;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Exemplos!");
+        Aluno aluno1 = new Aluno();
+        aluno1.nome = "Erick";
+        aluno1.notaBim1 = 3.7;
+        aluno1.notaBim2 = 6;
+        System.out.println("Aluno: "+aluno1.dados());
+
+        System.out.println("\n ");
+
+        PessoaFisica pf1 = new PessoaFisica();
+        pf1.nome = "Erick";
+        pf1.cpf = "12345678900";
+        pf1.dataNascimento = java.time.LocalDate.of(2002, 2, 20);
+        System.out.println("PessoaFisica: "+pf1.dados());
+
+
     }
 }
