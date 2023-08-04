e413816d9ddc0acaa4d0e81a533b6946670ef6ea
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-04 11:05:36 -0300

    Criando a classe conta da atividade

---
+++ src/atividade040823/Conta.java
@@ -0,0 +1,11 @@
+package atividade040823;
+
+public class Conta {
+    String cliente;
+    double saldo;
+    double limite;
+
+    public String sacar(){
+        return "Saldo: " + saldo;
+    }
+}
