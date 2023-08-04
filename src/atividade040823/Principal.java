d413b528991fd889af5fa85da5e8be442b00a7ef
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-04 11:23:41 -0300

    retirado o paremetro desconto, pois agora ele e um atributo

--- src/atividade040823/Principal.java
+++ src/atividade040823/Principal.java
@@ -5,15 +5,15 @@ public class Principal {
         Produto produto1 = new Produto();
         produto1.descricao = "Maça ";
         produto1.valor = 5;
-        double desconto1 = 0.5;
-        System.out.println(produto1.dados(produto1.valorDesconto(desconto1)));
+        produto1.percentualDesconto = 0.1;
+        System.out.println(produto1.dados  ());
 
         System.out.println("\n");
 
         Produto produto2 = new Produto();
         produto2.descricao = "Banana ";
         produto2.valor = 2;
-        double desconto2 = 0.5;
-        System.out.println(produto2.dados(desconto2));
+        produto2.percentualDesconto = 0.1;
+        System.out.println(produto2.dados());
     }
 }
