516882d7bc82485dc4fe84d00b4aac866f1a6541
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-04 11:17:05 -0300

    arrumando o parametro de desconto, colocado no principal modo doublo

--- src/atividade040823/Principal.java
+++ src/atividade040823/Principal.java
@@ -5,13 +5,15 @@ public class Principal {
         Produto produto1 = new Produto();
         produto1.descricao = "Maça ";
         produto1.valor = 5;
-        System.out.println(produto1.dados());
+        double desconto1 = 0.5;
+        System.out.println(produto1.dados(produto1.valorDesconto(desconto1)));
 
         System.out.println("\n");
 
         Produto produto2 = new Produto();
         produto2.descricao = "Banana ";
         produto2.valor = 2;
-        System.out.println(produto2.dados());
+        double desconto2 = 0.5;
+        System.out.println(produto2.dados(desconto2));
     }
 }
