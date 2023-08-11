08cbbe5f5d1f0daa20b98d7ac59b62ed5c32ac7b
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-11 16:52:02 -0300

    Adicionado o metodo aprovaçao na Classe Emprestimo

--- src/atividade110823/Emprestimo.java
+++ src/atividade110823/Emprestimo.java
@@ -9,17 +9,42 @@ public class Emprestimo {
         this.taxaJuros = taxaJuros;
     }
 
-    public String aprovaçao() {
+    public String aprovacao() {
         return (cliente.valorEmprestimo > cliente.rendaMensal * 0.3) ? "Emprestimo negado" : "Emprestimo aceito";
     }
 
-    public Double jurosMes(){
-        return valorEmprestimo + (valorEmprestimo * 0.1);
+    double valorTotal() {
+        double valorTotal = cliente.valorEmprestimo / Cliente.parcelas;
+        double valorSomado = 0;
+        for (int i = 0; i < Cliente.parcelas; i++) {
+            valorTotal += valorTotal * (taxaJuros / 100);
+            valorSomado += valorTotal;
+        }
+        valorSomado += cliente.valorEmprestimo;
+        return valorSomado;
+    }
+
+    double valorParcelas() {
+        double valorParcelas = valorTotal() / Cliente.parcelas;
+        return valorParcelas;
 
     }
 
-    public Double jurosAno(){
-        return (valorEmprestimo + jurosMes()) * 0.1 * Cliente.parcelas;
+    String totalParcelas() {
+        String parcelas = "";
+        for (int i = 0; i < Cliente.parcelas; i++) {
+            parcelas += (i + 1) + " Parcela: R$" + valorParcelas() + "\n";
+        }
+        return parcelas ;
     }
 
+    public String dados(){
+        String aprovacao = aprovacao();
+
+        if (aprovacao.equals("Emprestimo negado")) {
+            return aprovacao;
+        } else {
+            return  "\n" + aprovacao + "\n" + "Total de parcelas: " + Cliente.parcelas + "\n" + totalParcelas();
+        }
+    }
 }
