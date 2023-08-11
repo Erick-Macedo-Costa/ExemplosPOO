fe664e57cf2cb10bc8c2dfc2e9a4807c27883174
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-11 16:52:20 -0300

    Alterado a main

--- src/atividade110823/Principal.java
+++ src/atividade110823/Principal.java
@@ -2,8 +2,9 @@ package atividade110823;
 
 public class Principal {
     public static void main(String[] args) {
-        Cliente cliente = new Cliente("João", "Programador", 2000.0, 1000.0,10);
-        Emprestimo emprestimo = new Emprestimo(cliente, 1000.0);
+        Cliente cliente = new Cliente("João", "Programador", 3000.0, 500.0,10);
+        Emprestimo emprestimo = new Emprestimo(cliente, 10);
+        System.out.println(cliente.dados());
         System.out.println(emprestimo.dados());
     }
 }
