4eab650ea33510bd87db480868809ca13dcd994e
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-14 16:26:53 -0300

    Criando os metodos do Circulo da atividade2

--- src/atividade2/Circulo.java
+++ src/atividade2/Circulo.java
@@ -2,4 +2,17 @@ package atividade2;
 
 public class Circulo {
     double raio;
+
+    public double calcularArea() {
+        return Math.PI * Math.pow(raio, 2);
+    }
+
+    public double calcularPerimetro() {
+        return 2 * Math.PI * raio;
+    }
+
+    public String dadosCirculo() {
+    return "Dados do Circulo:" + "\nÁrea: " + calcularArea() + "\nPerímetro: " + calcularPerimetro();
+    }
+
 }
