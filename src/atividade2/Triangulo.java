be5f615fb49829962b9b15041b6a8852fd3468c9
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-15 15:29:54 -0300

    Criando o metodos de medidas do Triangulo

--- src/atividade2/Triangulo.java
+++ src/atividade2/Triangulo.java
@@ -3,4 +3,16 @@ package atividade2;
 public class Triangulo {
     double base;
     double altura;
+
+    public double area(){
+        return (base * altura) / 2;
+    }
+
+    public double perimetro(){
+        return base * 3;
+    }
+
+    public String dadosTriangulo(){
+        return "Dados do Triangulo:" + "\nÁrea: " + area() + "\nPerímetro: " + perimetro();
+    }
 }
