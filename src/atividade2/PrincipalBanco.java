e00dd22542abfcd9d1c2b88d05b7aefa8f746f5c
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-15 17:51:56 -0300

    Alrado o exemplo main

--- src/atividade2/PrincipalBanco.java
+++ src/atividade2/PrincipalBanco.java
@@ -2,7 +2,7 @@ package atividade2;
 
 public class PrincipalBanco {
     public static void main(String[] args) {
-        Cliente cliente = new Cliente("João", "Programador", 3000.0, 500.0,10);
+        Cliente cliente = new Cliente("João", "Programador", 3500.0, 1000.0,10);
         Emprestimo emprestimo = new Emprestimo(cliente, 10);
         System.out.println(cliente.dados());
         System.out.println(emprestimo.dados());
