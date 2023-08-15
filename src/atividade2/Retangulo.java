f90060b72ef08b43ea5c2d730ca459c8e68d4fec
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-15 15:25:29 -0300

    Criando os metodos de medidas do Retangulo

--- src/atividade2/Retangulo.java
+++ src/atividade2/Retangulo.java
@@ -4,4 +4,12 @@ public class Retangulo {
     double base;
     double altura;
 
+    public double area(){
+        return base * altura;
+    }
+
+    public double perimetro(){
+        return 2 * (base + altura);
+    }
+
 }
