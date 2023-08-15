f73ee0e3cfb1cd478d9b4f1de135e815c977f4c3
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-15 15:26:44 -0300

    Chamando o retangulo na main

--- src/atividade2/PrincipalFormulas.java
+++ src/atividade2/PrincipalFormulas.java
@@ -5,5 +5,10 @@ public class PrincipalFormulas {
         Circulo c1 = new Circulo();
         c1.raio = 5;
         System.out.println(c1.dadosCirculo());
+
+        Retangulo r1 = new Retangulo();
+        r1.base = 5;
+        r1.altura = 10;
+        System.out.println(r1.dadosRetangulo());
     }
 }
