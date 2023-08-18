67ff8b14be5b406e6c596aec82e708d6391ef185
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-18 16:29:09 -0300

    Criando a Classe Numeros da atividade 3, usado o Classe Random e adicionado o atribudo de tentativas

---
+++ src/atividade3/Numeros.java
@@ -0,0 +1,10 @@
+package atividade3;
+
+import java.util.Random;
+
+public class Numeros {
+    public int tentativas;
+    public Random random = new Random();
+    public int numero = random.nextInt(10);
+
+}
