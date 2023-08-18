1cfe157c4bc646f9d7e715b5681f142f8babd3a2
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-18 16:52:06 -0300

    Criando a resoluçado da atividade, sem usao While, como o professor disse em sala, ultilizei o if mesmo, se acerta, acertou, else if dica e else dica, tipo retorno void, pois nao precisa me rertona nada

--- src/atividade3/Numeros.java
+++ src/atividade3/Numeros.java
@@ -1,10 +1,30 @@
 package atividade3;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Numeros {
     public int tentativas;
     public Random random = new Random();
-    public int numero = random.nextInt(10);
+    public int numero = random.nextInt(100);
+
+    public void jogada() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um numero de 0 a 100: ");
+        int chute = scanner.nextInt();
+        if (chute == numero) {
+            System.out.println("Acertou!");
+        } else if (chute > numero) {
+            System.out.println("Errou, e menor que: " + chute);
+            jogada();
+        } else {
+            System.out.println("Errou, e maior que: " + chute);
+            jogada();
+        }
+        tentativas++;
+    }
+
+   
+
 
 }
