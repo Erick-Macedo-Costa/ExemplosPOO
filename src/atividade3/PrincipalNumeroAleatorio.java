5f42c4943838123497497dfbaf29f7c5de2d0379
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-18 16:53:24 -0300

    Chamado a classe Numero, para a main e iniciado o jogo e retornado os dados

--- src/atividade3/PrincipalNumeroAleatorio.java
+++ src/atividade3/PrincipalNumeroAleatorio.java
@@ -3,5 +3,7 @@ package atividade3;
 public class PrincipalNumeroAleatorio {
     public static void main(String[] args) {
         Numeros numeros = new Numeros();
+        numeros.jogada();
+        System.out.println(numeros.dados());
     }
 }
