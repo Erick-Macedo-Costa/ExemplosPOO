34649dd27f2a338d31591cfd724c8913e0fae6ff
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-22 21:46:38 -0300

    Adicionado o Do while no projeto

--- src/atividade3/Numero.java
+++ src/atividade3/Numero.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Numero {
-    public int tentativas;
+    public int tentativas=0;
     public Random random = new Random();
     public int numero = random.nextInt(100);
 
@@ -25,6 +25,21 @@ public class Numero {
         tentativas++;
     }
 
+//    public void jogada() {
+//        Scanner scanner = new Scanner(System.in);
+//        int chute=0;
+//        do {
+//            System.out.println("Digite um numero de 0 a 100: ");
+//            chute = scanner.nextInt();
+//            if (chute == numero)
+//                System.out.println("Acertou!");
+//             else if (chute > numero)
+//                System.out.println("Errou, e menor que: " + chute);
+//            else
+//                System.out.println("Errou, e maior que: " + chute);
+//            tentativas++;
+//        } while (chute != numero);
+//    }
     public String dados() {
         return "Numero sorteado: " + numero + "\n" + "Quantidade de tentativas: " + tentativas;
     }
