15eab19113a34ddd648ea76a9c79230574981c1a
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-25 08:57:22 -0300

    Criando os atributos Estado

--- src/atividade4/Estado.java
+++ src/atividade4/Estado.java
@@ -1,5 +1,10 @@
 package atividade4;
 
 public class Estado {
+    private String nome;
+    private String sigla;
 
+    public String dados() {
+        return "Nome: " + nome + "Sigla: " + sigla;
+    }
 }
