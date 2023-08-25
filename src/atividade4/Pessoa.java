fc6e222604a5fa59d7fe8ea42d2f8b5af947d5fe
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-25 08:58:35 -0300

    Criando os atributos Pessoa

--- src/atividade4/Pessoa.java
+++ src/atividade4/Pessoa.java
@@ -5,5 +5,16 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Pessoa {
+    private Endereco endereco;
+    private String nome;
+    private DateFormat dataNascimento;
 
+
+    public double idade() {
+        return 2023 - dataNascimento.getCalendar().get(Calendar.YEAR);
+    }
+
+    public String dados() {
+        return "Nome: " + nome + "Idade: " + idade();
+    }
 }
