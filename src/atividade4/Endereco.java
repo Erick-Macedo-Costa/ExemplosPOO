c649405dbd102aaa1dd8eb89729e838659c4e096
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-08-25 08:54:23 -0300

    Criando os atributos Endereco

--- src/atividade4/Endereco.java
+++ src/atividade4/Endereco.java
@@ -1,5 +1,14 @@
 package atividade4;
 
 public class Endereco {
+    private Cidade cidade;
+    private String logradouro;
+    private String barrio;
+    private int cep;
+    private String complemento;
 
+
+    public String dados() {
+        return "Logradouro: " + logradouro + "Bairro: " + barrio + "CEP: " + cep + "Complemento: " + complemento;
+    }
 }
