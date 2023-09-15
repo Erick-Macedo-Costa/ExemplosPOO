c1a19eb91fb4a6f27f4d6b01229ab6f30e57f555
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-09-15 09:46:46 -0300

    Criando Produto, com seus atributodos e metodos, conforme o Diagrama

---
+++ src/atividade5/Produto.java
@@ -0,0 +1,10 @@
+package atividade5;
+
+public class Produto {
+    String descricao;
+    double preco;
+
+    String dados() {
+        return "Descrição: " + descricao + ", Preço Unitario: " + preco;
+    }
+}
